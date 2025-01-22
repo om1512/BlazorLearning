a076942e363f45a4b2507b32c8661b896f032e89
Om Dhingani <ODhingani@corp.mm-software.com>
2025-01-22 11:26:12 +0530

    Learned So far

---
+++ LearnBlazorApp/Helper/JsRuntimeExtenstion.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.JSInterop;
+
+namespace LearnBlazorApp.Helper
+{
+    public static class JsRuntimeExtenstion
+    {
+        public static async Task ToastrSuccess(this IJSRuntime iJsRuntime, string message)
+        {
+            await iJsRuntime.InvokeVoidAsync("showToastr", "success", message);
+        }
+
+        public static async Task ToastrError(this IJSRuntime iJsRuntime, string message)
+        {
+            await iJsRuntime.InvokeVoidAsync("showToastr", "error", message);
+        }
+    }
+}
---
+++ LearnBlazorApp/wwwroot/Js/common.js
@@ -0,0 +1,29 @@
+﻿
+
+window.showToastr = function (type, message) {
+    if (type == "success") {
+        toastr.success(message);
+    }
+    if (type == "error") {
+        toastr.error(message);
+    }
+}
+
+window.displaySwal = function (type, message) {
+    if (type == "success") {
+        Swal.fire({
+            title: "Drag me!",
+            icon: "success",
+            draggable: true
+        });
+
+    }
+    if (type == "error") {
+        Swal.fire({
+            title: 'Error!',
+            text: 'Do you want to continue',
+            icon: 'error',
+            confirmButtonText: 'Cool'
+        });
+    }
+}
